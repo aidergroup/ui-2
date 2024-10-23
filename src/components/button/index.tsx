3815c7e3ba628dd1cfe4ce5ce993f0aea1cf5fe8
homosape <irakli.ghachava@delori.io>
2024-10-23 23:33:58 +0400

    Added custom theme config

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,6 +1,10 @@
 import { forwardRef } from "react";
 import { Button, ButtonProps } from "@mui/material";
 
-export default forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
-    return <Button {...props} ref={ref} />;
+export default forwardRef<HTMLButtonElement, ButtonProps>(({ children, ...props }, ref) => {
+    return (
+        <Button {...props} ref={ref}>
+            {children}
+        </Button>
+    );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as Button } from "./components/button";
 export { default as Typography } from "./components/typography";
+export { default as theme } from "./theme";
